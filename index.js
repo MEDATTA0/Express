416b1134161f71191403604356db4561ebf2606e
MEDATTA0 <ibrahimdjahadi46@gmail.com>
2024-10-22 21:19:20 +0200

    creating branch

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
+
+app.listen((port) => {
+  console.log("Server listening on port ", port);
+});
