90897b05fb30b1631a244a5380193e43204670c0
MEDATTA0 <ibrahimdjahadi46@gmail.com>
2024-10-23 23:03:07 +0200

    cleaning userRoutes and todosRoutes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,12 @@ const pool = require("../db/db_connect");
 
 const router = express.Router();
 
-router.post("/login");
+router.post("/login", (req, res) => {});
+
+router.get("/reset", (req, res) => {});
+
+router.patch("/update", (req, res) => {});
+
+router.post("/access", (req, res) => {});
 
 module.exports = router;
