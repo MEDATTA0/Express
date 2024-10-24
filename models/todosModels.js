e4fad613fa85f4f915a966d70400ebc04aafb850
MEDATTA0 <ibrahimdjahadi46@gmail.com>
2024-10-24 17:51:28 +0200

    modifying todoModels.js

--- models/todosModels.js
+++ models/todosModels.js
@@ -1,18 +1,37 @@
 const pool = require("../db/db_connect");
 
+exports.getAllTodos = async () => {
+  let db_conn;
+  let response;
+  try {
+    db_conn = await pool.getConnection();
+    const sqlQuery = "SELECT * FROM tasks";
+    const rows = await db_conn.execute(sqlQuery);
+    response = rows;
+  } catch (err) {
+    response = err;
+    console.log("error : ", err);
+  } finally {
+    if (db_conn) db_conn.release();
+    return response;
+  }
+};
+
 exports.getTodos = async (userId) => {
   const userId = req.params.userId;
   let db_conn;
+  let response;
   try {
     db_conn = await pool.getConnection();
     const sqlQuery = "SELECT * FROM tasks WHERE userId=? ";
     const rows = await db_conn.execute(sqlQuery, [userId]);
-    if (rows === 0) res.json({ response: "Task empty !" });
+    response = rows;
   } catch (err) {
-    res.status(500).json({ error: "Error while fetching server !" });
+    response = err;
     console.log("Error : ", err);
   } finally {
     if (pool) db_conn.release();
+    return response;
   }
 };
 
@@ -21,7 +40,7 @@ exports.createTodo = async (userId, title, task_description, isCompleted) => {
   if (!title || !task_description || typeof isCompleted === "undefined") {
     return res.status(400).json({ error: "Please fill fields." });
   }
-
+  let response;
   let db_conn;
   try {
     db_conn = await pool.getConnection();
@@ -32,16 +51,12 @@ exports.createTodo = async (userId, title, task_description, isCompleted) => {
       isCompleted,
       userId,
     ]);
-
-    res.json({
-      message: "Task added successfully",
-      taskId: result.task_id,
-    });
-    console.log("Todo added successfully");
+    response = result;
   } catch (err) {
-    res.status(500).json({ error: "Server while adding task" });
+    response = err;
     console.log("Error : ", err);
   } finally {
-    if (db_conn_conn_conn_conn) db_conn.release();
+    if (db_conn) db_conn.release();
+    return response;
   }
 };
